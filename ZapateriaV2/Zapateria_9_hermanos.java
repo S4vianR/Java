9881af636c051c033367c1d4cea56e024f4ad748
Joshua Salcido <tavaresjoshua8@gmail.com>
2022-11-15 16:27:46 -0600

    Ultimo Commit de la Zapateria V2. Refactorizacion de la clase Main

--- ZapateriaV2/Zapateria_9_hermanos.java
+++ ZapateriaV2/Zapateria_9_hermanos.java
@@ -2,119 +2,126 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Zapateria_9_hermanos {
-    static ArrayList<Empleado> empleados = new ArrayList<>();
-    static Scanner s1 = new Scanner(System.in);
-    static Scanner pNombres = new Scanner(System.in);
-    static Scanner pString = new Scanner(System.in);
-    static int op = 0;
-    static char opC;
-    static double sueldo = 0.00;
-    static long numEmpleado = 0;
-    static String nombreEmpleado, puesto, ruta, encargado;
+    private static ArrayList<Empleado> empleados = new ArrayList<>();
+    private static Scanner scannerDouble = new Scanner(System.in);
+    private static Scanner scannerString = new Scanner(System.in);
+    private static int op = 0;
+    private static char opC;
+    private static double sueldo = 0.00;
+    private static long numEmpleado = 0;
+    private static String nombreEmpleado, puesto, ruta, encargado;
 
     public static void main(String[] args) {
         limpiarConsola();
         do {
             System.out.println(
-                    "\nBienvenido\n1.Registro de empleado de sistemas\n2.Registro de empleado repartidor\n3.Registro de encargados de la zapateria\n4.Listar empleados registrados\n5.Pagar quincena a todos\n6.Salir");
-            op = s1.nextInt();
-            switch (op) {
-                case 1: {
-                    registroS();
+                    "\nBienvenido\n"
+                        + "1.Registro de empleado de sistemas\n"
+                        + "2.Registro de empleado repartidor\n"
+                        + "3.Registro de encargados de la zapateria\n"
+                        + "4.Listar empleados registrados\n"
+                        + "5.Pagar quincena a todos\n"
+                        + "6.Salir");
+            op = scannerDouble.nextInt();
+            limpiarConsola();
+            switch(op) {
+                case 1:
+                case 2:
+                case 3:
+                    agregarEmpleado(op);
                     break;
-                }
-                case 2: {
-                    registroR();
-                    break;
-                }
-                case 3: {
-                    registroE();
-                    break;
-                }
-                case 4: {
-                    listar();
-                    break;
-                }
-                case 5: {
-                    pago();
-                    break;
-                }
-                case 6: {
-                    System.out.println("Gracias, vuelva pronto");
-                    break;
-                }
-                default: {
-                    System.out.println("Error");
-                    break;
-                }
+                case 4: listar(); break;
+                case 5: pago(); break;
+                case 6: System.out.println("Gracias, vuelva pronto"); break;
+                default: System.out.println("Opcion no valida");
             }
         } while (op != 6);
     }
 
-    public static void registroS() {
-        limpiarConsola();
+    private static void agregarEmpleado(int op) {
         do {
-            System.out.println("Cual es el nombre del empleado:");
-            nombreEmpleado = pNombres.nextLine();
-            System.out.println("Puesto (Diseñador de bases de datos o Programador):");
-            puesto = pString.nextLine();
-            do {
-                System.out.println("Cual es el sueldo del empleado");
-                sueldo = s1.nextDouble();
-            } while (sueldo < 1000);
-            do {
-                System.out.println("Numero de empleado:");
-                numEmpleado = s1.nextLong();
-            } while (numEmpleado < 0);
-            Sistemas regSistemas = new Sistemas(nombreEmpleado, numEmpleado, sueldo, puesto);
-            empleados.add(regSistemas);
+            pedirDatosEmpleado();
+            switch(op) {
+                case 1: registroS(); break;
+                case 2: registroR(); break;
+                case 3: registroE(); break;
+                default: System.out.println("Error");
+            }
             System.out.println("\nDesea agregar a otro empleado (s/n):");
-            opC = s1.next().charAt(0);
+            opC = scannerDouble.next().charAt(0);
         } while (opC == 's' || opC == 'S');
+    }
+
+    public static String pedirNombre() {
+        System.out.println("Cual es el nombre del empleado:");
+        return scannerString.nextLine();
+    }
+
+    public static long pedirNumEmpleado() {
+        long numEmpleado;
+        do {
+            System.out.println("Numero de empleado:");
+            numEmpleado = scannerDouble.nextLong();
+        } while (numEmpleado < 0);
+        return numEmpleado;
+    }
+    
+    public static double pedirSueldo() {
+        double sueldo;
+        do {
+            System.out.println("Cual es el sueldo del empleado");
+            sueldo = scannerDouble.nextDouble();
+        } while (sueldo < 1000);
+        return sueldo;
+    }
+    
+    public static void pedirDatosEmpleado() {
+        nombreEmpleado = pedirNombre();
+        System.out.println("");
+        sueldo = pedirSueldo();
+        System.out.println("");
+        numEmpleado = pedirNumEmpleado();
+    }
+    
+    public static String predirPuesto() {
+        System.out.println("Puesto (Diseñador de bases de datos o Programador):");
+        return scannerString.nextLine();
+    }
+
+    public static void registroS() {
+        System.out.println("");
+        puesto = predirPuesto();
+        empleados.add(
+            new Sistemas(nombreEmpleado, numEmpleado, sueldo, puesto)
+        );
+    }
+
+    public static String pedirRuta() {
+        System.out.println("Ruta:");
+        return scannerString.nextLine();
     }
 
     public static void registroR() {
-        limpiarConsola();
-        do {
-            System.out.println("Cual es el nombre del empleado:");
-            nombreEmpleado = pNombres.nextLine();
-            System.out.println("Ruta (Zona Centro, Colonia del Real, Colonia El Pontificio, etc):");
-            ruta = pString.nextLine();
-            do {
-                System.out.println("Cual es el sueldo del empleado");
-                sueldo = s1.nextDouble();
-            } while (sueldo < 1000);
-            do {
-                System.out.println("Numero de empleado:");
-                numEmpleado = s1.nextLong();
-            } while (numEmpleado < 0);
-            Repartidor regRepartidor = new Repartidor(nombreEmpleado, numEmpleado, sueldo, ruta);
-            empleados.add(regRepartidor);
-            System.out.println("\nDesea agregar a otro empleado (s/n):");
-            opC = s1.next().charAt(0);
-        } while (opC == 's' || opC == 'S');
+        System.out.println("");
+        ruta = pedirRuta();
+        
+        empleados.add(
+            new Repartidor(nombreEmpleado, numEmpleado, sueldo, ruta)
+        );
+    }
+
+    public static String pedirEncargado() {
+        System.out.println("Encargado: ");
+        return scannerString.nextLine();
     }
 
     public static void registroE() {
-        limpiarConsola();
-        do {
-            System.out.println("Cual es el nombre del empleado:");
-            nombreEmpleado = pNombres.nextLine();
-            System.out.println("De que es encargado (Calzado de hombre, mujer o niños)");
-            encargado = pString.nextLine();
-            do {
-                System.out.println("Cual es el sueldo del empleado");
-                sueldo = s1.nextDouble();
-            } while (sueldo < 1000);
-            do {
-                System.out.println("Numero de empleado:");
-                numEmpleado = s1.nextLong();
-            } while (numEmpleado < 0);
-            Actualiza actualiza = new Actualiza(nombreEmpleado, numEmpleado, sueldo, encargado);
-            empleados.add(actualiza);
-            System.out.println("\nDesea agregar a otro empleado (s/n):");
-            opC = s1.next().charAt(0);
-        } while (opC == 's' || opC == 'S');
+        System.out.println("");
+        encargado = pedirEncargado();
+
+        empleados.add(
+            new Actualiza(nombreEmpleado, numEmpleado, sueldo, encargado)
+        );
     }
 
     public static void pago() {
@@ -125,7 +132,6 @@ public class Zapateria_9_hermanos {
         for (Empleado p : empleados) {
             System.out.println("A " + p.getNombre() + " se le ha pagado " + p.pago() + " pesos");
         }
-
     }
 
     public static void listar() {
